a0efe1d9332bcfc0ad4785401bb305bba4941d6f
Renan <uzumaki.renandroid@gmail.com>
2021-03-15 15:02:23 -0300

    02 - Configurando o React e Bootstrap

---
+++ 02 - Configurando React/confusion/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorant Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;

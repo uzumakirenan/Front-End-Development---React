c1e76852ae73e945294448af68e79264f94b5bdd
Renan <uzumaki.renandroid@gmail.com>
2021-03-16 16:19:50 -0300

    05 - Metodos Lifecycle

---
+++ 05 - Metodos Lifecycle/confusion/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+import MenuC from './components/MenuCompClass';
+import MenuF from './components/MenuComponentFunction';
+import MenuSF from './components/MenuCompSharedFunc';
+import MenuSC from './components/MenuCompSharedClass';
+import { DISHES } from './shared/dishes'
+
+
+function App(props) {
+
+  
+
+  return (
+    <div>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorant Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <MenuSC dishes={DISHES} />     
+
+    </div>
+  );
+}
+
+export default App;
